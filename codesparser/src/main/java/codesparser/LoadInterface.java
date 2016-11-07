479ccc758207563f3ed83fd3530e15db67dde5af
Karl Nicholas <Karl.Nicholas@outlook.com>
2016-11-06 17:01:01 -0800

    Update/restore CALoad.

--- codesparser/src/main/java/codesparser/LoadInterface.java
+++ codesparser/src/main/java/codesparser/LoadInterface.java
@@ -3,6 +3,6 @@ package codesparser;
 import java.io.File;
 
 public interface LoadInterface {
-	public void loadCode(File codesDir, File xmlcodes, File index, File indextaxo) throws Exception;
+	public void loadCode(File codesDir, File index, File indextaxo) throws Exception;
 	public void createXMLCodes(File codesDir, File loadPath) throws Exception;
 }
